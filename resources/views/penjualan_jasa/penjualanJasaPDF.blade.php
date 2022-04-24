510601da7ede0cd8da804e02b734c50ef13b331f
Sakti <saktiajadeh29@gmail.com>
2022-04-25 00:48:01 +0800

    test

--- resources/views/penjualan_jasa/penjualanJasaPDF.blade.php
+++ resources/views/penjualan_jasa/penjualanJasaPDF.blade.php
@@ -14,81 +14,74 @@
     }
 
     #table-data td, #table-data th {
-        border: 1px solid black;
+        border: 1px solid #ddd;
+        padding: 5px;
     }
 </style>
 
 <body>
 <div class="invoice-box">
-    <table cellpadding="0" cellspacing="0">
+    <table cellpadding="0" cellspacing="0" width="100%">
         <tr class="top">
             <td colspan="2">
                 <table>
                     <tr>
                         <td class="title">
-                            {{-- <img src="{{ asset('logo.png') }}" style="width:100%; max-width:300px; height: 50px; objec-fit: contain; display: none;"> --}}
+                            <h1>Bengkel Murah Jaya</h1>
                         </td>
                     </tr>
                 </table>
             </td>
+          <td align="right">
+            Denpasar, {{ $penjualan_jasa->tanggal }}
+          </td>
         </tr>
     </table>
 
-
-        <table border="0" id="table-data" width="100%">
-            <tr>
-                <td width="70px">Invoice ID</td>
-                <td width="">: {{ $penjualan_jasa->id }}</td>
-                <td width="30px">Created</td>
-                <td>: {{ $penjualan_jasa->tanggal }}</td>
-            </tr>
-
-            <tr>
-                <td>Telepon</td>
-                <td>: {{ $penjualan_jasa->customer->telepon }}</td>
-                <td>Alamat</td>
-                <td>: {{ $penjualan_jasa->customer->alamat }}</td>
-            </tr>
-
-            <tr>
-                <td>Nama</td>
-                <td>: {{ $penjualan_jasa->customer->nama }}</td>
-                <td>Email</td>
-                <td>: {{ $penjualan_jasa->customer->email }}</td>
-            </tr>
-
-            <tr>
-                <td>Jasa</td>
-                <td >: {{ $penjualan_jasa->jasa->nama }}</td>
-                <td>Keterangan</td>
-                <td >: {{ $penjualan_jasa->jasa->keterangan }}</td>
-            </tr>
-
-            <tr>
-                <td>Total Bayar</td>
-                <td colspan="3">: {{ $penjualan_jasa->jasa->harga }}</td>
-            </tr>
-
-        </table>
-
-        {{--<hr  size="2px" color="black" align="left" width="45%">--}}
-
-
-        <table border="0" width="100%">
-            <tr align="right">
-                <td>Hormat Kami</td>
-            </tr>
-        </table>
-
-    {{-- <table border="0" width="10%">
-        <tr align="right">
-            <td><img src="https://upload.wikimedia.org/wikipedia/en/f/f4/Timothy_Spall_Signature.png" width="100px" height="100px"></td>
+    <table border="0" width="30%" style="margin-top: 10px;">
+        <tr>
+            <td>
+            Pelanggan
+          </td>
+          <td>
+            : {{ $penjualan_jasa->customer->nama }}
+          </td>
         </tr>
-    </table> --}}
+        <tr>
+            <td>
+                Telepon
+            </td>
+            <td>
+                : {{ $penjualan_jasa->customer->telepon }}
+            </td>
+        </tr>
+    </table>
 
-    <table border="0" width="100%">
+    <table border="0" id="table-data" width="100%">
+        <tr style="background: #000; color: #fff;">
+          <td align="center" colspan="4"><strong>Invoice ID {{ $penjualan_jasa->id }}</strong></td>
+        </tr>
+
+        <tr align="center">
+            <td width="46%" align="left"><strong>Jasa</strong></td>
+            <td width="18%"><strong>Keterangan</strong></td>
+            <td width="18%"><strong>Harga</strong></td>
+            <td width="18%"><strong>Total</strong></td>
+        </tr>
+        <tr align="center">
+            <td width="46%" align="left">{{ $penjualan_jasa->jasa->nama }}</td>
+            <td width="18%">{{ $penjualan_jasa->jasa->keterangan }}</td>
+            <td width="18%">Rp. {{ $penjualan_jasa->jasa->harga }}</td>
+            <td width="18%">Rp. {{ $penjualan_jasa->jasa->harga }}</td>
+        </tr>
+    </table>
+
+    <table border="0" width="100%" style="margin-top: 20px; clear: both;">
         <tr align="right">
-            <td>PT asik asik bengkel</td>
+            <td>Hormat Kami</td>
+        </tr>
+        <tr align="right">
+            <td>Bengkel Murah Jaya</td>
         </tr>
     </table>
 </div>
--- resources/views/product_keluar/productKeluarPDF.blade.php
+++ resources/views/product_keluar/productKeluarPDF.blade.php
@@ -14,83 +14,74 @@
     }
 
     #table-data td, #table-data th {
-        border: 1px solid black;
+        border: 1px solid #ddd;
+        padding: 5px;
     }
 </style>
 
 <body>
 <div class="invoice-box">
-    <table cellpadding="0" cellspacing="0">
+    <table cellpadding="0" cellspacing="0" width="100%">
         <tr class="top">
             <td colspan="2">
                 <table>
                     <tr>
                         <td class="title">
-                            {{-- <img src="{{ asset('logo.png') }}" style="width:100%; max-width:300px; height: 50px; objec-fit: contain; display: none;"> --}}
+                            <h1>Bengkel Murah Jaya</h1>
                         </td>
                     </tr>
                 </table>
             </td>
+          <td align="right">
+            Denpasar, {{ $product_keluar->tanggal }}
+          </td>
         </tr>
     </table>
 
-
-        <table border="0" id="table-data" width="100%">
-            <tr>
-                <td width="70px">Invoice ID</td>
-                <td width="">: {{ $product_keluar->id }}</td>
-                <td width="30px">Created</td>
-                <td>: {{ $product_keluar->tanggal }}</td>
-            </tr>
-
-            <tr>
-                <td>Telepon</td>
-                <td>: {{ $product_keluar->customer->telepon }}</td>
-                <td>Alamat</td>
-                <td>: {{ $product_keluar->customer->alamat }}</td>
-            </tr>
-
-            <tr>
-                <td>Nama</td>
-                <td>: {{ $product_keluar->customer->nama }}</td>
-                <td>Email</td>
-                <td>: {{ $product_keluar->customer->email }}</td>
-            </tr>
-
-            <tr>
-                <td>Barang</td>
-                <td >: {{ $product_keluar->product->nama }}</td>
-                <td>Quantity</td>
-                <td >: {{ $product_keluar->qty }}</td>
-            </tr>
-
-            <tr>
-                <td>Harga</td>
-                <td >: {{ $product_keluar->product->harga }}</td>
-                <td>Total Bayar</td>
-                <td >: {{ $product_keluar->qty * $product_keluar->product->harga }}</td>
-            </tr>
-
-        </table>
-
-        {{--<hr  size="2px" color="black" align="left" width="45%">--}}
-
-
-        <table border="0" width="100%">
-            <tr align="right">
-                <td>Hormat Kami</td>
-            </tr>
-        </table>
-
-    {{-- <table border="0" width="10%">
-        <tr align="right">
-            <td><img src="https://upload.wikimedia.org/wikipedia/en/f/f4/Timothy_Spall_Signature.png" width="100px" height="100px"></td>
+    <table border="0" width="30%" style="margin-top: 10px;">
+        <tr>
+            <td>
+            Pelanggan
+          </td>
+          <td>
+            : {{ $product_keluar->customer->nama }}
+          </td>
         </tr>
-    </table> --}}
+        <tr>
+            <td>
+                Telepon
+            </td>
+            <td>
+                : {{ $product_keluar->customer->telepon }}
+            </td>
+        </tr>
+    </table>
 
-    <table border="0" width="100%">
+    <table border="0" id="table-data" width="100%">
+        <tr style="background: #000; color: #fff;">
+          <td align="center" colspan="4"><strong>Invoice ID {{ $product_keluar->id }}</strong></td>
+        </tr>
+
+        <tr align="center">
+            <td width="58%" align="left"><strong>Barang</strong></td>
+            <td width="6%"><strong>Jumlah</strong></td>
+            <td width="22%"><strong>Harga Satuan</strong></td>
+            <td width="22%"><strong>Total</strong></td>
+        </tr>
+        <tr align="center">
+            <td width="58%" align="left">{{ $product_keluar->product->nama }}</td>
+            <td width="6%">{{ $product_keluar->qty }}</td>
+            <td width="18%">Rp. {{ $product_keluar->product->harga }}</td>
+            <td width="18%">Rp. {{ $product_keluar->qty * $product_keluar->product->harga }}</td>
+        </tr>
+    </table>
+
+    <table border="0" width="100%" style="margin-top: 20px; clear: both;">
         <tr align="right">
-            <td>PT asik asik bengkel</td>
+            <td>Hormat Kami</td>
+        </tr>
+        <tr align="right">
+            <td>Bengkel Murah Jaya</td>
         </tr>
     </table>
 </div>
